6150a62a0a1c9ab163527198018479acac8a576a
«Ales» <alopusansky@gmail.com>
2023-08-02 21:15:17 +0300

    diff script

--- Семинар 2/assets/scripts/script.js
+++ Семинар 2/assets/scripts/script.js
@@ -1,22 +1,22 @@
-// function Meating() {
-//     let name = prompt("Введите свое имя: ");
-//     alert("Привет, " + name);
-// }
+function Meating() {
+    let name = prompt("Введите свое имя: ");
+    alert("Привет, " + name);
+}
 
-// Meating();
+Meating();
 
-// let age = parseInt(prompt("Введите свой возраст: "));
-// switch (age) {
-//     case 18:
-//         alert("Вы совершеннолетний, все можно! ")
-//         break;
-//     case 10:
-//         alert("Вам нужно учить уроки! ")
-//         break;
-//     case 30:
-//         alert("Ложитесь спать, завтра на работу! ")
-//         break;
-//     default:
-//         alert("Мы не знаем что вам делать! ")
-//         break;
-// }
+let age = parseInt(prompt("Введите свой возраст: "));
+switch (age) {
+    case 18:
+        alert("Вы совершеннолетний, все можно! ")
+        break;
+    case 10:
+        alert("Вам нужно учить уроки! ")
+        break;
+    case 30:
+        alert("Ложитесь спать, завтра на работу! ")
+        break;
+    default:
+        alert("Мы не знаем что вам делать! ")
+        break;
+}
