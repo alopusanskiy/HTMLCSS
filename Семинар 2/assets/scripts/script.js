4d0153e0055e673fca18d8f28294a844f2661957
«Ales» <alopusansky@gmail.com>
2023-08-02 21:09:44 +0300

    add scripts in project

---
+++ Семинар 2/assets/scripts/script.js
@@ -0,0 +1,22 @@
+// function Meating() {
+//     let name = prompt("Введите свое имя: ");
+//     alert("Привет, " + name);
+// }
+
+// Meating();
+
+// let age = parseInt(prompt("Введите свой возраст: "));
+// switch (age) {
+//     case 18:
+//         alert("Вы совершеннолетний, все можно! ")
+//         break;
+//     case 10:
+//         alert("Вам нужно учить уроки! ")
+//         break;
+//     case 30:
+//         alert("Ложитесь спать, завтра на работу! ")
+//         break;
+//     default:
+//         alert("Мы не знаем что вам делать! ")
+//         break;
+// }
